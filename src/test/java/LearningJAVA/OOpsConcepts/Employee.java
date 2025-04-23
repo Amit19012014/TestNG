456e393179af35acf721881f3ef4ec43dcfa2839
Amit19012014 <Amit19012014@gmail.com>
2025-04-24 00:10:08 +0530

    23 April 2025

---
+++ src/test/java/LearningJAVA/OOpsConcepts/Employee.java
@@ -0,0 +1,39 @@
+package LearningJAVA.OOpsConcepts;
+
+public class Employee {
+
+    //Variables
+    int eid;
+    String name;
+    String job;
+    int sal;
+
+    //Method
+    void display() {
+        System.out.println(eid);
+        System.out.println(name);
+        System.out.println(job);
+        System.out.println(sal);
+    }
+
+
+//    public static void main(String[] args) {
+//
+//        Employee emp = new Employee();
+//        emp.name="Amit";
+//        emp.eid = 001;
+//        emp.job = "QA";
+//        emp.sal = 100000000;
+//        emp.display();
+//
+//        System.out.println("=======================");
+//        Employee emp1 = new Employee();
+//        emp1.name ="Yogesh";
+//        emp1.eid = 002;
+//        emp1.sal = 10000;
+//        emp1.job = "sales";
+//        emp1.display();
+//
+//
+//    }
+}
---
+++ src/test/java/LearningJAVA/OOpsConcepts/Student.java
@@ -0,0 +1,14 @@
+package LearningJAVA.OOpsConcepts;
+
+public class Student {
+
+    String name;
+    int roll_no;
+    char grade;
+
+    void display(){
+        System.out.println(name+ " "+roll_no+" "+grade);
+//        System.out.println(roll_no);
+//        System.out.println(city);
+    }
+}
