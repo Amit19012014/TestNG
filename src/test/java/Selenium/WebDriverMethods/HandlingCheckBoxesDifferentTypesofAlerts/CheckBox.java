6ff9689e44ae324353c98195690f015723fbab4c
Amit19012014 <Amit19012014@gmail.com>
2025-05-16 22:08:28 +0530

    16-May-2025

---
+++ src/test/java/Selenium/WebDriverMethods/HandlingCheckBoxesDifferentTypesofAlerts/CheckBox.java
@@ -0,0 +1,59 @@
+package Selenium.WebDriverMethods.HandlingCheckBoxesDifferentTypesofAlerts;
+
+import com.google.j2objc.annotations.Weak;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CheckBox {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com");
+        driver.manage().window().maximize();
+
+
+        //Select specific checkbox
+//        driver.findElement(By.xpath("//input[@id='sunday']")).click();
+
+        //Select all the checkboxes
+        List<WebElement> box = driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
+
+//        for (int i=0;i< box.size();i++){
+//            box.get(i).click();
+//        }
+
+//        for (WebElement B : box ){
+//            B.click();
+//        }
+
+//      //2  Select last 3 checkboxes
+        //total number of checkboxes - how many checkboxes want to select = starting index
+        //7-3=4(Starting index )
+//
+//        //3 select last 3 checkboxes
+//        for (int i =4;i<box.size();i++){
+//            box.get(i).click();
+//        }
+
+        //4 select first 3 checkboxes
+//        for (int j = 0; j < 3; j++) {
+//            box.get(j).click();
+//        }
+
+        //5 unselect checkboxes if they are selected
+        for (int j = 0; j < 3; j++) {
+            box.get(j).click();
+        }
+        Thread.sleep(3000);
+        for (int j = 0; j< box.size() ; j++) {
+            if (box.get(j).isSelected()){
+                box.get(j).click();
+            }
+
+        }
+
+    }
+}
---
+++ src/test/java/Selenium/WebDriverMethods/NavigationCommands/Assignment2.java
@@ -0,0 +1,69 @@
+
+
+package Selenium.WebDriverMethods.NavigationCommands;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+public class Assignment2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1) Launch browser and go to the website
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+        // 2) Search for a term
+        driver.findElement(By.xpath("//input[@class='wikipedia-search-input']")).sendKeys("Amit");
+        driver.findElement(By.xpath("//input[@class='wikipedia-search-button']")).click();
+
+        // Give time for results to load
+        Thread.sleep(2000);
+
+        // 3) Get all search result links
+        List<WebElement> links = driver.findElements(By.xpath("//div[@id='wikipedia-search-result-link']/a"));
+
+        // 4) Count number of links
+        System.out.println("Number of search result links: " + links.size());
+
+        // 5) Click each link - open in new tab or window
+        for (WebElement link : links) {
+            link.click();
+            Thread.sleep(1000); // Small wait to let new window/tab open
+        }
+
+        // 6) Get all window handles
+        Set<String> windowHandles = driver.getWindowHandles();
+        System.out.println("Number of browser windows opened: " + windowHandles.size());
+
+        // 7) Close a specific window (e.g., window with title containing "Amit")
+        for (String handle : windowHandles) {
+            driver.switchTo().window(handle);
+            String title = driver.getTitle();
+            System.out.println("Window Title: " + title);
+
+            if (title.contains("Amit")) {
+                System.out.println("Closing window with title: " + title);
+                driver.close();
+                break;
+            }
+        }
+
+        // Switch back to the original window
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+            break;
+        }
+
+        // Optionally quit the entire session
+        // driver.quit();
+    }
+}
