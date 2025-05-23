91e6f789ba00df6cbf4e67387a8a17c84ea21416
Amit19012014 <Amit19012014@gmail.com>
2025-05-24 00:00:23 +0530

    23-May-2025

---
+++ src/test/java/Selenium/KeyboardActionsSlidersTabsAndWindows/OpenTheLinkInNewTab.java
@@ -0,0 +1,39 @@
+package Selenium.KeyboardActionsSlidersTabsAndWindows;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class OpenTheLinkInNewTab {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement Register = driver.findElement(By.xpath("//a[@class='ico-register']"));
+        Actions act = new Actions(driver);
+
+        // Control+Registation Link
+        act.keyDown(Keys.CONTROL).click(Register).keyUp(Keys.CONTROL).perform();
+
+        //Switching to registrtion page
+        List<String> id = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(id.get(1)); // switch to registration page
+
+        driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("Amit");
+
+        driver.switchTo().window(id.get(0)); // Switch to home page
+
+        driver.findElement(By.xpath("//input[@name=\"q\"]")).sendKeys("Tshirt");
+
+    }
+}
