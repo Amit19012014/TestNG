5c8ea9009ad4c9f587d122a3522e33aca4b0af2f
Amit19012014 <Amit19012014@gmail.com>
2025-05-25 23:34:31 +0530

    25-May-2025

---
+++ src/test/java/Selenium/JavascriptExecutor_ScrollingPages_UploadFiles/ScrollingPage.java
@@ -0,0 +1,46 @@
+package Selenium.JavascriptExecutor_ScrollingPages_UploadFiles;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ScrollingPage {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html");
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+       //1 Scroll down the page by pixel number.
+//        js.executeScript("window.scrollBy(0,1500)","");
+//        System.out.println(js.executeScript("return window.pageYOffset;"));
+//         Thread.sleep(2000);
+
+         //2 Scroll the page till element is visible
+//        WebElement element = driver.findElement(By.xpath("//strong[contains(text(),'Community poll')]"));
+//
+//        js.executeScript("arguments[0].scrollIntoView();",element);
+//        System.out.println(js.executeScript("return window.pageYOffset;"));
+//        Thread.sleep(2000);
+
+        //3 Scroll page till end of the page
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+        System.out.println(js.executeScript("return window.pageYOffset;"));
+        Thread.sleep(2000);
+
+        //Scrolling up to initial position
+        js.executeScript("window.scrollBy(0,-document.body.scrollHeight)");
+        System.out.println(js.executeScript("return window.pageYOffset;"));
+        Thread.sleep(2000);
+
+
+
+
+    }
+}
---
+++ src/test/java/Selenium/MouseHoverAndActionVsActions/Assignment.java
@@ -0,0 +1,37 @@
+package Selenium.MouseHoverAndActionVsActions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class Assignment {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        Actions act = new Actions(driver);
+
+        WebElement doubleClickElement = driver.findElement(By.xpath("//button[contains(text(),'Copy Text')]"));
+
+        act.doubleClick(doubleClickElement).build().perform();
+
+
+
+        // Source element
+        WebElement source = driver.findElement(By.xpath("//p[contains(text(),'Drag me to my target')]"));
+        //Target element
+        WebElement target = driver.findElement(By.xpath("//p[contains(text(),'Drop here')]"));
+
+        act.dragAndDrop(source,target).build().perform();
+
+
+    }
+
+}
