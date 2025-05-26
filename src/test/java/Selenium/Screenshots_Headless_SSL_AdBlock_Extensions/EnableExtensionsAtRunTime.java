b00d3d4b715a5900a54445d813faa99d9ed071cd
Amit19012014 <Amit19012014@gmail.com>
2025-05-26 13:37:47 +0530

    26-May-2025

---
+++ src/test/java/Selenium/Screenshots_Headless_SSL_AdBlock_Extensions/EnableExtensionsAtRunTime.java
@@ -0,0 +1,35 @@
+package Selenium.Screenshots_Headless_SSL_AdBlock_Extensions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+
+public class EnableExtensionsAtRunTime {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+
+        File file = new File("C:\\Users\\amit1\\OneDrive\\Desktop\\Java\\TestNG\\src\\test\\java\\CRX Extensions\\SelectorsHub-Chrome-Web-Store.crx");
+        File file2 = new File("C:\\Users\\amit1\\OneDrive\\Desktop\\Java\\TestNG\\src\\test\\java\\CRX Extensions\\uBlock-Origin-Chrome-Web-Store.crx");
+        options.addExtensions(file,file2);
+
+        WebDriver driver = new ChromeDriver(options);
+
+
+        // Open the URL
+        driver.get("https://text-compare.com/");
+
+        // Validate title should be "Your Store"
+        String act_title = driver.getTitle();
+
+        if (act_title.equals("Your Store")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+        Thread.sleep(10000);
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/Selenium/Screenshots_Headless_SSL_AdBlock_Extensions/IncognitoMode.java
@@ -0,0 +1,29 @@
+package Selenium.Screenshots_Headless_SSL_AdBlock_Extensions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class IncognitoMode {
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        WebDriver driver = new ChromeDriver(options);
+
+
+        // Open the URL
+        driver.get("https://demo.opencart.com.gr");
+
+        // Validate title should be "Your Store"
+        String act_title = driver.getTitle();
+
+        if (act_title.equals("Your Store")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+
+        driver.quit();
+    }
+}
